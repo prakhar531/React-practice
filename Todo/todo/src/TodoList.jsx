958aea83d8f712f91dbeddcaf05aac3604cf99ca
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-01-12 17:43:53 +0530

    Todo app- Added mark ad done, not done and delete functionalities

--- Todo/todo/src/TodoList.jsx
+++ Todo/todo/src/TodoList.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
 export default function TodoList() {
+
     //creating array of objects to store task and unique id which is generate using uuid npm package
-    let [todos, setTodos] = useState([{ task: "coding", id: uuidv4() }]);
+    let [todos, setTodos] = useState([{ task: "coding", id: uuidv4(), isDone: false }]);
     let [newTodo, setNewTodo] = useState("");
 
     let addNewTodo = () => {
         setTodos((prevTodo) => {
-            return [...prevTodo, { task: newTodo, id: uuidv4() }];
+            return [...prevTodo, { task: newTodo, id: uuidv4(), isDone: false }];
         });
         setNewTodo("");
     };
@@ -21,11 +22,74 @@ export default function TodoList() {
         setTodos((prevTodo) => prevTodo.filter((prevTodo) => prevTodo.id != id));
     };
 
+    let markAsDoneAll = () => {
+        setTodos((prevTodo) => (
+            prevTodo.map((todo) => {
+                return {
+                    ...todo,
+                    isDone: true,
+                }
+            })
+        ));
+    };
+
+    let markAsDoneone = (id) => {
+        setTodos((prevTodo) => (
+            prevTodo.map((todo) => {
+                if (todo.id == id) {
+                    return {
+                        ...todo,
+                        isDone: true,
+                    }
+                } else {
+                    return todo;
+                }
+
+            })
+        ));
+    };
+
+    let markAsNotDoneAll = () => {
+        setTodos((prevTodo) => (
+            prevTodo.map((todo) => {
+                return {
+                    ...todo,
+                    isDone: false,
+                }
+            })
+        ));
+    };
+
+    let markAsNotDoneone = (id) => {
+        setTodos((prevTodo) => (
+            prevTodo.map((todo) => {
+                if (todo.id == id) {
+                    return {
+                        ...todo,
+                        isDone: false,
+                    }
+                } else {
+                    return todo;
+                }
+
+            })
+        ));
+    };
+
+
+
+    let textStyle = {
+        textDecoration: 'lineThrough',
+    };
+
     return (
         <div>
-            <input type="text" placeholder="Enter some task" value={newTodo} onChange={updateTodoValue} />
-            <br />
-            <br />
+
+            <h2>Todo App</h2><br />
+            <hr />
+
+            <h4>Add Tasks</h4>
+            <input type="text" placeholder="Enter some task" value={newTodo} onChange={updateTodoValue} />&nbsp;&nbsp;
             <button onClick={addNewTodo}>Add task</button>
             <br />
             <br />
@@ -35,13 +99,22 @@ export default function TodoList() {
                 {
                     todos.map((todo) => (
                         <li key={todo.id}>
-                            <span>{todo.task}</span>
-                            &nbsp;&nbsp;
-                            <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+                            <span style={todo.isDone ? { textDecorationLine: "line-through" } : {}}>
+                                {todo.task}
+                            </span>&nbsp;&nbsp;
+
+                            <button onClick={() => deleteTodo(todo.id)}>Delete</button>&nbsp;&nbsp;
+                            {todo.isDone ? <button onClick={() => markAsNotDoneone(todo.id)}>Mark as Not Done</button> : <button onClick={() => markAsDoneone(todo.id)}>Mark as Done</button>}
+                            <br />
+                            <br />
                         </li>
                     ))
                 }
             </ul>
+            <br />
+            <br />
+            <button onClick={markAsDoneAll}> Mark All as Done</button>&nbsp;&nbsp;
+            <button onClick={markAsNotDoneAll}> Mark All as Not Done</button>
         </div>
     );
 };
